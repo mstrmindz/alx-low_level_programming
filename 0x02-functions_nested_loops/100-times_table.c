4c3850f14c94b0b4cdd0a323572099c77d5203f7
mstrmindz <tunjexi@yahoo.com>
2022-09-13 09:59:10 -0700

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,18 +1,11 @@
-#include "main.h"
+#include <stdio.h>
+int main() {
+  int n, i;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+  for (i = 1; i <= 100; ++i) {
+    printf("%d * %d = %d \n", n, i, n * i);
+  }
+  return 0;
 
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_times_table(3);
-    _putchar('\n');
-    print_times_table(5);
-    _putchar('\n');
-    print_times_table(98);
-    _putchar('\n');
-    print_times_table(12);  
-    return (0);
-}
+
